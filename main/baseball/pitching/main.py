ac779e87993b86a6428c3487c5dd07d69cc39c97
Andy Nilson <andy@andynilson.com>
2022-03-16 22:38:40 -0700

    added padres charts for 3/15 game

--- main/baseball/pitching/main.py
+++ main/baseball/pitching/main.py
@@ -4,41 +4,36 @@ from heatMap import *
 from determineValue import *
 from util import *
 
+
+
+
+
+
+
+
 #lancers
 plotAndSaveAllTypesIndividually("13")
 plotAndSaveAllTypesIndividually("20")
 
 #padres
-# updateWithInput("opp13")
+plotAndSaveAllTypesIndividually("opp13")
+plotAndSaveAllTypesIndividually("opp6")
 
 
-# generateHeatmapsFromPlayer("13", True)
-# generateHeatmapsFromPlayer("20", True)
-# generateHeatmapsFromPlayer("20", True)
-
-# saveToImage("23")
-
-# plotScatterWithType("23", "H")
-# saveToImage("Hits23")
-# plotScatterWithType("23", "B")
-# saveToImage("Balls23")
-# plotScatterWithType("23", "S")
-# saveToImage("Strikes23")
 
 
-# updateWithInput("16")
-# saveToImage("16")
-
-# plotScatterWithType("16", "H")
-# saveToImage("Hits16")
-# plotScatterWithType("16", "B")
-# saveToImage("Balls16")
-# plotScatterWithType("16", "S")
-# saveToImage("Strikes16")
 
 
-# generateHeatmapsFromPlayer("23", False)
-# generateHeatmapsFromPlayer("16", False)
+
+
+
+
+
+
+
+
+
+
 
 
 show()
