7c925eda8d6ac48d3dc416c0ddc9e48b0b119794
Andy Nilson <andy@andynilson.com>
2022-03-16 22:13:11 -0700

    added lancer charts and data for 3/15 game

--- main/baseball/pitching/util.py
+++ main/baseball/pitching/util.py
@@ -3,10 +3,11 @@ from heatMap import *
 
 def plotAndSaveAllTypesIndividually(playerNum):
     types = ['S', 'H', 'B']
+    typeNames = ["strikes", "hits", "balls"]
     data = openPLayer(playerNum)
     updateWithInput(playerNum)
     saveToImage(playerNum)
     generateHeatmaps(data, playerNum, True)
-    for i in types:
-        plotScatterWithType(playerNum, i)
-        saveToImage(playerNum)
+    for i in range(len(types)):
+        plotScatterWithType(playerNum, types[i])
+        saveToImage(typeNames[i] + playerNum)
